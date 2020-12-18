3d8a02ccbc17002cdebdc5c02516e522c05809a2
nxshen <michael7105@gmail.com>
2020-12-18 20:57:49 +0000

    test

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
 
 import boto3
 
-print("test\n")
+client = boto3.client('kinesis-video-media')
+
+response = client.create_signaling_channel(
+    ChannelName='string',
+    ChannelType='SINGLE_MASTER',
+    SingleMasterConfiguration={
+        'MessageTtlSeconds': 123
+    },
+    Tags=[
+        {
+            'Key': 'string',
+            'Value': 'string'
+        },
+    ]
+)
