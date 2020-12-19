e36f496fc4855a49be7201f33e211d559f7585d8
nxshen <michael7105@gmail.com>
2020-12-19 16:31:47 +0800

    test

--- stream_to_video.py
+++ stream_to_video.py
@@ -1,6 +1,29 @@
 import boto3
 import cv2
 import time
+from botocore.exceptions import ClientError
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 STREAM_NAME = "MyKinesisVideoStream"
 
@@ -24,6 +47,9 @@ url = kvam.get_hls_streaming_session_url(
 
 
 vcap = cv2.VideoCapture(url)
+s3 = boto3.client('s3')
+BUCKET_NAME = "pistreambucket"
+OBJECT_NAME = "currentFrame"
 
 while(True):
     # Capture frame-by-frame
@@ -33,6 +59,8 @@ while(True):
         # Display the resulting frame
         # cv2.imshow('frame', frame)
         cv2.imwrite("frame.jpg", frame)
+        with open("frame.jpg", "rb") as f:
+            s3.upload_fileobj(f, BUCKET_NAME, OBJECT_NAME)
 
         # Press q to close the video windows before it ends if you want
         if cv2.waitKey(22) & 0xFF == ord('q'):
