e031f902793d908c2afab26ba0c32b93ab95d701
nxshen <michael7105@gmail.com>
2020-12-27 12:03:17 +0000

    test

--- random_update.py
+++ random_update.py
@@ -4,20 +4,21 @@ import time
 import string
 import os
 import requests
-import MySQLdb
+import mysql.connector
+from mysql.connector import Error
 
 # Obtain connection string information from the portal
-connection= MySQLdb.connect(
+connection= mysql.connector.connect(
     host='localhost',
-    port = 3306,
+    port=3306,
     user='root',
     passwd=os.environ['MYSQL_ROOT_PASSWORD'],
     db ='pi_parking_monitor',)
 
 cursor = connection.cursor()
 
-cursor.execute("update parking_space set lisence_plate_head='ACE' lisence_plate_tail='2468' where camera_id = 'A02'")
+cursor.execute("update parking_space set lisence_plate_head='ACE' lisence_plate_tail='2468' where camera_id='A02'")
 
-cursor.close()
 connection.commit()
+cursor.close()
 connection.close()
